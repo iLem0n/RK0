2834d1689e05bc70b4efd7801b27269736c169c9
Peter C. Glade (iLem0n) <peter.c.glade@googlemail.com>
2018-09-13 19:54:56 +0200

    Initial Commit

---
+++ FridgeWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FridgeWatch
+//
+//  Created by Peter Christian Glade on 13.09.18.
+//  Copyright © 2018 Peter Christian Glade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
